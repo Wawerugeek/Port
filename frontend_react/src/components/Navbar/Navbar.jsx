8fe41569b8d568f15f281e52d996c55951eb5f64
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-23 19:00:11 +0300

    Navbar: js and scss complete

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import './Navbar.scss';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { images } from '../../assets';
 
-export default Navbar
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        {/* loop through elements in navigation bar */}
+        {['home', 'about', 'contact', 'work', 'skills', 'testimonials'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}> {item} </a>
+
+          </li>
+        ))}
+      </ul>
+
+      <div className='app__navbar-menu'>
+        <ul>
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+          {
+          toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                {['home', 'about', 'contact', 'work', 'skills', 'testimonials'].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}> {item} </a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          )
+}
+        </ul>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
