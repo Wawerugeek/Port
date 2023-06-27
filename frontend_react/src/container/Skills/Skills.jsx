b77b589451105d306454d722d1751fd280ca81f2
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-28 02:30:58 +0300

    skills and experiences section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import ReactTooltip from 'react-tooltip';
 import { motion } from 'framer-motion';
+import { Tooltip } from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
-
 import './Skills.scss';
 
 const Skills = () => {
@@ -12,8 +11,8 @@ const Skills = () => {
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "skills"]';
-    const skillsQuery = '*[_type == "experiences"]';
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
 
     client.fetch(query).then((data) => {
       setExperiences(data);
@@ -26,30 +25,64 @@ const Skills = () => {
 
   return (
     <>
-      <h2 className='head-text'>skills & Experience</h2>
+      <h2 className="head-text">Skills & Experiences</h2>
 
-      <div className='app__skills-container'>
-        <motion.div className='app__skills-list'>
-          {/* mapping the skills */}
-
-          { skills.map((skill) => (
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
             <motion.div
-              whileInView={{ opacity: [0, 1]}}
-              transition={ { duration: 0.5}}
-              className='app__skills-item app__flex'
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
               key={skill.name}
             >
-
-              <div className='app__flex' style={{ backgroundColor: skill.bgColor}}>
-
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
                 <img src={urlFor(skill.icon)} alt={skill.name} />
-                
               </div>
-               
+              <p className="p-text">{skill.name}</p>
             </motion.div>
-
           ))}
-          </motion.div>
+        </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <Tooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </Tooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
